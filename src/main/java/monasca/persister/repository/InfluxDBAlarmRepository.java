9d5e1f4cbb157bea37ed7c1a2f00b4f17842b0e8
Jonathan Halterman <jhalterman@gmail.com>
2014-09-26 16:48:48 -0700

    WIP - Persist metrics along with alarm-state-history records in Influx
    Change-Id: If4c3742f2ee6282da3457fde99b7efa5537b0408

--- src/main/java/monasca/persister/repository/InfluxDBAlarmRepository.java
+++ src/main/java/monasca/persister/repository/InfluxDBAlarmRepository.java
@@ -1,47 +1,51 @@
 /*
  * Copyright (c) 2014 Hewlett-Packard Development Company, L.P.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
- * implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 
 package monasca.persister.repository;
 
-import com.hpcloud.mon.common.event.AlarmStateTransitionedEvent;
-import monasca.persister.configuration.MonPersisterConfiguration;
-
-import com.codahale.metrics.Meter;
-import com.codahale.metrics.Timer;
-import com.google.inject.Inject;
-
 import io.dropwizard.setup.Environment;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import monasca.persister.configuration.MonPersisterConfiguration;
 
 import org.influxdb.dto.Serie;
 import org.influxdb.dto.Serie.Builder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
+import com.codahale.metrics.Meter;
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.Timer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.google.inject.Inject;
+import com.hpcloud.mon.common.event.AlarmStateTransitionedEvent;
 
 public class InfluxDBAlarmRepository extends InfluxRepository implements AlarmRepository {
-
-  static final Logger logger = LoggerFactory.getLogger(InfluxDBAlarmRepository.class);
+  private static final Logger logger = LoggerFactory.getLogger(InfluxDBAlarmRepository.class);
   private static final String ALARM_STATE_HISTORY_NAME = "alarm_state_history";
+  private static final String[] COLUMN_NAMES = {"tenant_id", "alarm_id", "metrics", "old_state",
+      "new_state", "reason", "reason_data", "time"};
+  static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
-  private final String[] colNamesStringArry = {"tenant_id", "alarm_id", "old_state", "new_state",
-      "reason", "reason_data", "time"};
+  static {
+    OBJECT_MAPPER
+        .setPropertyNamingStrategy(PropertyNamingStrategy.CAMEL_CASE_TO_LOWER_CASE_WITH_UNDERSCORES);
+  }
 
   protected final Timer flushTimer;
 
@@ -53,10 +57,10 @@ public class InfluxDBAlarmRepository extends InfluxRepository implements AlarmRe
   public InfluxDBAlarmRepository(MonPersisterConfiguration configuration, Environment environment) {
     super(configuration, environment);
     this.flushTimer =
-        this.environment.metrics().timer(this.getClass().getName() + "." + "flush-timer");
+        this.environment.metrics().timer(MetricRegistry.name(getClass(), "flush-timer"));
     this.alarmStateHistoryMeter =
         this.environment.metrics().meter(
-            this.getClass().getName() + "." + "alarm_state_history-meter");
+            MetricRegistry.name(getClass(), "alarm_state_history-meter"));
   }
 
   @Override
@@ -67,9 +71,7 @@ public class InfluxDBAlarmRepository extends InfluxRepository implements AlarmRe
 
   @Override
   public void flush() {
-
     try {
-
       if (this.alarmStateTransitionedEventList.isEmpty()) {
         logger.debug("There are no alarm state transition events to be written to the influxDB");
         logger.debug("Returning from flush");
@@ -82,14 +84,15 @@ public class InfluxDBAlarmRepository extends InfluxRepository implements AlarmRe
       final Builder builder = new Serie.Builder(ALARM_STATE_HISTORY_NAME);
       logger.debug("Created serie: {}", ALARM_STATE_HISTORY_NAME);
 
-      builder.columns(this.colNamesStringArry);
+      builder.columns(COLUMN_NAMES);
 
       if (logger.isDebugEnabled()) {
-        logColumnNames(this.colNamesStringArry);
+        logColumnNames(COLUMN_NAMES);
       }
 
       for (AlarmStateTransitionedEvent alarmStateTransitionedEvent : alarmStateTransitionedEventList) {
         builder.values(alarmStateTransitionedEvent.tenantId, alarmStateTransitionedEvent.alarmId,
+            OBJECT_MAPPER.writeValueAsString(alarmStateTransitionedEvent.metrics),
             alarmStateTransitionedEvent.oldState, alarmStateTransitionedEvent.newState,
             alarmStateTransitionedEvent.stateChangeReason, "{}",
             alarmStateTransitionedEvent.timestamp);
