885112b6055e9c713bf4b99ade74c2c8524aea41
Deklan Dieterly <deklan.dieterly@hp.com>
2014-10-03 09:38:12 -0600

    Refactor code to remove duplication
    Create an abstract super class to handle the logic in the run method.
    
    Change-Id: I8f4f1598d724cdbffa08235995f055f42674941a

--- monasca_persister/persister.py
+++ monasca_persister/persister.py
@@ -23,8 +23,11 @@
 
     Also able to use Openstack service to start the persister.
 """
+
+import abc
 from datetime import datetime
 import json
+import six
 import sys
 import threading
 import urllib
@@ -41,38 +44,28 @@ import service
 
 LOG = log.getLogger(__name__)
 
-kafka_opts = [
-    cfg.StrOpt('uri'),
-    cfg.StrOpt('alarm_history_group_id'),
-    cfg.StrOpt('alarm_history_topic'),
-    cfg.StrOpt('alarm_history_consumer_id'),
-    cfg.StrOpt('alarm_history_client_id'),
-    cfg.IntOpt('alarm_batch_size'),
-    cfg.IntOpt('alarm_max_wait_time_seconds'),
-    cfg.StrOpt('metrics_group_id'),
-    cfg.StrOpt('metrics_topic'),
-    cfg.StrOpt('metrics_consumer_id'),
-    cfg.StrOpt('metrics_client_id'),
-    cfg.IntOpt('metrics_batch_size'),
-    cfg.IntOpt('metrics_max_wait_time_seconds')
-]
+kafka_opts = [cfg.StrOpt('uri'), cfg.StrOpt('alarm_history_group_id'),
+              cfg.StrOpt('alarm_history_topic'),
+              cfg.StrOpt('alarm_history_consumer_id'),
+              cfg.StrOpt('alarm_history_client_id'),
+              cfg.IntOpt('alarm_batch_size'),
+              cfg.IntOpt('alarm_max_wait_time_seconds'),
+              cfg.StrOpt('metrics_group_id'), cfg.StrOpt('metrics_topic'),
+              cfg.StrOpt('metrics_consumer_id'),
+              cfg.StrOpt('metrics_client_id'),
+              cfg.IntOpt('metrics_batch_size'),
+              cfg.IntOpt('metrics_max_wait_time_seconds')]
 
-kafka_group = cfg.OptGroup(name='kafka',
-                           title='kafka')
+kafka_group = cfg.OptGroup(name='kafka', title='kafka')
 
 cfg.CONF.register_group(kafka_group)
 cfg.CONF.register_opts(kafka_opts, kafka_group)
 
-influxdb_opts = [
-    cfg.StrOpt('database_name'),
-    cfg.StrOpt('ip_address'),
-    cfg.StrOpt('port'),
-    cfg.StrOpt('user'),
-    cfg.StrOpt('password')
-]
+influxdb_opts = [cfg.StrOpt('database_name'), cfg.StrOpt('ip_address'),
+                 cfg.StrOpt('port'), cfg.StrOpt('user'),
+                 cfg.StrOpt('password')]
 
-influxdb_group = cfg.OptGroup(name='influxdb',
-                              title='influxdb')
+influxdb_group = cfg.OptGroup(name='influxdb', title='influxdb')
 cfg.CONF.register_group(influxdb_group)
 cfg.CONF.register_opts(influxdb_opts, influxdb_group)
 
@@ -91,7 +84,7 @@ def main():
         alarm_persister = AlarmPersister(cfg.CONF)
         alarm_persister.start()
     except Exception:
-        log.exception('Persister encountered fatal error. Shutting down.')
+        LOG.exception('Persister encountered fatal error. Shutting down.')
 
 
 class Persister(os_service.Service):
@@ -105,234 +98,205 @@ class Persister(os_service.Service):
         main()
 
 
-class AlarmPersister(threading.Thread):
+@six.add_metaclass(abc.ABCMeta)
+class AbstractPersister(threading.Thread):
+
+    def __init__(self, consumer, influxdb_client, max_wait_time_secs,
+                 batch_size):
+        super(AbstractPersister, self).__init__()
+
+        self.__consumer = consumer
+        self.__influxdb_client = influxdb_client
+        self.__max_wait_time_secs = max_wait_time_secs
+        self.__batch_size = batch_size
+
+        self.__json_body = []
+        self.__last_flush = datetime.now()
+
+    @abc.abstractmethod
+    def process_message(self, message):
+        pass
+
+    def run(self):
+
+        def flush(self):
+            if self.__json_body:
+                self.__influxdb_client.write_points(self.__json_body)
+                self.__consumer.commit()
+            self.__last_flush = datetime.now()
+            self.__json_body = []
+
+        try:
+
+            while (True):
+
+                delta_time = datetime.now() - self.__last_flush
+                if (delta_time.seconds > self.__max_wait_time_secs):
+                    flush(self)
+
+                for message in self.__consumer:
+                    self.__json_body.append(self.process_message(message))
+                    if len(self.__json_body) % self.__batch_size == 0:
+                        flush(self)
+
+        except Exception:
+            LOG.exception(
+                'Persister encountered fatal exception processing messages')
+            raise
+
+
+class AlarmPersister(AbstractPersister):
     """Class for persisting alarms.
     """
 
     def __init__(self, conf):
-        threading.Thread.__init__(self)
-        self.conf = conf
+        kafka = KafkaClient(conf.kafka.uri)
+        consumer = SimpleConsumer(kafka, conf.kafka.alarm_history_group_id,
+                                  conf.kafka.alarm_history_topic,
+                                  auto_commit=False, iter_timeout=1)
 
-    def run(self):
+        influxdb_client = InfluxDBClient(conf.influxdb.ip_address,
+                                         conf.influxdb.port,
+                                         conf.influxdb.user,
+                                         conf.influxdb.password,
+                                         conf.influxdb.database_name)
 
-        count = 0
-        json_body = []
-        last_flush = datetime.now()
+        max_wait_time_secs = conf.kafka.alarm_max_wait_time_seconds
+        batch_size = conf.kafka.alarm_batch_size
+        super(AlarmPersister, self).__init__(consumer, influxdb_client,
+                                             max_wait_time_secs, batch_size)
 
-        try:
+    def process_message(self, message):
+        LOG.debug(message.message.value.decode('utf8'))
 
-            kafka = KafkaClient(self.conf.kafka.uri)
-            consumer = SimpleConsumer(kafka,
-                                      self.conf.kafka.alarm_history_group_id,
-                                      self.conf.kafka.alarm_history_topic,
-                                      auto_commit=False, iter_timeout=1)
+        decoded = json.loads(message.message.value)
+        LOG.debug(json.dumps(decoded, sort_keys=True, indent=4))
 
-            influxdb_client = InfluxDBClient(self.conf.influxdb.ip_address,
-                                             self.conf.influxdb.port,
-                                             self.conf.influxdb.user,
-                                             self.conf.influxdb.password,
-                                             self.conf.influxdb.database_name)
-            while (True):
+        actions_enabled = decoded['alarm-transitioned']['actionsEnabled']
+        LOG.debug('actions enabled: %s', actions_enabled)
 
-                delta_time = datetime.now() - last_flush
-                if (delta_time.seconds >
-                        self.conf.kafka.alarm_max_wait_time_seconds):
-                    if json_body:
-                        influxdb_client.write_points(json_body)
-                        consumer.commit()
-                    last_flush = datetime.now()
-                    count = 0
-                    json_body = []
+        alarm_description = decoded['alarm-transitioned']['alarmDescription']
+        LOG.debug('alarm description: %s', alarm_description)
 
-                for message in consumer:
-                    LOG.debug(message.message.value.decode('utf8'))
+        alarm_id = decoded['alarm-transitioned']['alarmId']
+        LOG.debug('alarm id: %s', alarm_id)
 
-                    decoded = json.loads(message.message.value)
-                    LOG.debug(
-                        json.dumps(decoded, sort_keys=True, indent=4))
+        alarm_definition_id = decoded['alarm-transitioned'][
+            'alarmDefinitionId']
+        LOG.debug('alarm definition id: %s', alarm_definition_id)
 
-                    actions_enabled = decoded['alarm-transitioned'][
-                        'actionsEnabled']
-                    LOG.debug('actions enabled: %s', actions_enabled)
+        metrics = decoded['alarm-transitioned']['metrics']
+        LOG.debug('metrics: %s', metrics)
 
-                    alarm_description = decoded['alarm-transitioned'][
-                        'alarmDescription']
-                    LOG.debug('alarm description: %s', alarm_description)
+        alarm_name = decoded['alarm-transitioned']['alarmName']
+        LOG.debug('alarm name: %s', alarm_name)
 
-                    alarm_id = decoded['alarm-transitioned']['alarmId']
-                    LOG.debug('alarm id: %s', alarm_id)
+        new_state = decoded['alarm-transitioned']['newState']
+        LOG.debug('new state: %s', new_state)
 
-                    alarm_definition_id = decoded['alarm-transitioned'][
-                        'alarmDefinitionId']
-                    LOG.debug('alarm definition id: %s', alarm_definition_id)
+        old_state = decoded['alarm-transitioned']['oldState']
+        LOG.debug('old state: %s', old_state)
 
-                    metrics = decoded['alarm-transitioned']['metrics']
-                    LOG.debug('metrics: %s', metrics)
+        state_change_reason = decoded['alarm-transitioned'][
+            'stateChangeReason']
+        LOG.debug('state change reason: %s', state_change_reason)
 
-                    alarm_name = decoded['alarm-transitioned']['alarmName']
-                    LOG.debug('alarm name: %s', alarm_name)
+        tenant_id = decoded['alarm-transitioned']['tenantId']
+        LOG.debug('tenant id: %s', tenant_id)
 
-                    new_state = decoded['alarm-transitioned']['newState']
-                    LOG.debug('new state: %s', new_state)
+        time_stamp = decoded['alarm-transitioned']['timestamp']
+        LOG.debug('time stamp: %s', time_stamp)
 
-                    old_state = decoded['alarm-transitioned']['oldState']
-                    LOG.debug('old state: %s', old_state)
+        data = {"points": [[time_stamp, '{}', tenant_id.encode('utf8'),
+                            alarm_id.encode('utf8'),
+                            alarm_definition_id.encode('utf8'),
+                            json.dumps(metrics, ensure_ascii=False).encode(
+                                'utf8'), old_state.encode('utf8'),
+                            new_state.encode('utf8'),
+                            state_change_reason.encode('utf8')]],
+                "name": 'alarm_state_history',
+                "columns": ["time", "reason_data", "tenant_id", "alarm_id",
+                            "alarm_definition_id", "metrics", "old_state",
+                            "new_state", "reason"]}
 
-                    state_change_reason = decoded['alarm-transitioned'][
-                        'stateChangeReason']
-                    LOG.debug('state change reason: %s',
-                              state_change_reason)
-
-                    tenant_id = decoded['alarm-transitioned']['tenantId']
-                    LOG.debug('tenant id: %s', tenant_id)
-
-                    time_stamp = decoded['alarm-transitioned']['timestamp']
-                    LOG.debug('time stamp: %s', time_stamp)
-
-                    data = {"points": [
-                        [time_stamp, '{}', tenant_id.encode('utf8'),
-                         alarm_id.encode('utf8'),
-                         alarm_definition_id.encode('utf8'),
-                         json.dumps(metrics, ensure_ascii=False).encode('utf8'), old_state.encode('utf8'),
-                         new_state.encode('utf8'),
-                         state_change_reason.encode('utf8')]],
-                            "name": 'alarm_state_history',
-                            "columns": ["time", "reason_data", "tenant_id",
-                                        "alarm_id", "alarm_definition_id",
-                                        "metrics", "old_state",
-                                        "new_state",
-                                        "reason"]}
-
-                    LOG.debug(data)
-                    json_body.append(data)
-
-                    count += 1
-                    if count % self.conf.kafka.alarm_batch_size == 0:
-                        influxdb_client.write_points(json_body)
-                        consumer.commit()
-                        last_flush = datetime.now()
-                        count = 0
-                        json_body = []
-
-        except Exception:
-            LOG.exception(
-                'Persister encountered fatal exception processing alarms')
-            raise
+        LOG.debug(data)
+        return data
 
 
-class MetricPersister(threading.Thread):
+class MetricPersister(AbstractPersister):
     """Class for persisting metrics.
     """
 
     def __init__(self, conf):
-        threading.Thread.__init__(self)
-        self.conf = conf
 
-    def run(self):
+        kafka = KafkaClient(conf.kafka.uri)
+        consumer = SimpleConsumer(kafka, conf.kafka.metrics_group_id,
+                                  conf.kafka.metrics_topic, auto_commit=False,
+                                  iter_timeout=1)
 
-        count = 0
-        json_body = []
-        last_flush = datetime.now()
+        influxdb_client = InfluxDBClient(conf.influxdb.ip_address,
+                                         conf.influxdb.port,
+                                         conf.influxdb.user,
+                                         conf.influxdb.password,
+                                         conf.influxdb.database_name)
 
-        try:
+        max_wait_time_secs = conf.kafka.metrics_max_wait_time_seconds
+        batch_size = conf.kafka.metrics_batch_size
+        super(MetricPersister, self).__init__(consumer, influxdb_client,
+                                              max_wait_time_secs, batch_size)
 
-            kafka = KafkaClient(self.conf.kafka.uri)
-            consumer = SimpleConsumer(kafka,
-                                      self.conf.kafka.metrics_group_id,
-                                      self.conf.kafka.metrics_topic,
-                                      auto_commit=False, iter_timeout=1)
+    def process_message(self, message):
 
-            influxdb_client = InfluxDBClient(self.conf.influxdb.ip_address,
-                                             self.conf.influxdb.port,
-                                             self.conf.influxdb.user,
-                                             self.conf.influxdb.password,
-                                             self.conf.influxdb.database_name)
+        LOG.debug(message.message.value.decode('utf8'))
 
-            while (True):
+        decoded = json.loads(message.message.value)
+        LOG.debug(json.dumps(decoded, sort_keys=True, indent=4))
 
-                delta_time = datetime.now() - last_flush
-                if (delta_time.seconds >
-                        self.conf.kafka.metrics_max_wait_time_seconds):
-                    if json_body:
-                        influxdb_client.write_points(json_body)
-                        consumer.commit()
-                    last_flush = datetime.now()
-                    count = 0
-                    json_body = []
+        metric_name = decoded['metric']['name']
+        LOG.debug('name: %s', metric_name)
 
-                for message in consumer:
-                    LOG.debug(message.message.value.decode('utf8'))
+        creation_time = decoded['creation_time']
+        LOG.debug('creation time: %s', creation_time)
 
-                    decoded = json.loads(message.message.value)
-                    LOG.debug(
-                        json.dumps(decoded, sort_keys=True, indent=4))
+        region = decoded['meta']['region']
+        LOG.debug('region: %s', region)
 
-                    metric_name = decoded['metric']['name']
-                    LOG.debug('name: %s', metric_name)
+        tenant_id = decoded['meta']['tenantId']
+        LOG.debug('tenant id: %s', tenant_id)
 
-                    creation_time = decoded['creation_time']
-                    LOG.debug('creation time: %s', creation_time)
+        dimensions = {}
+        if 'dimensions' in decoded['metric']:
+            for dimension_name in decoded['metric']['dimensions']:
+                dimensions[dimension_name] = (
+                    decoded['metric']['dimensions'][dimension_name])
+                LOG.debug('dimension: %s : %s', dimension_name,
+                          dimensions[dimension_name])
 
-                    region = decoded['meta']['region']
-                    LOG.debug('region: %s', region)
+        time_stamp = decoded['metric']['timestamp']
+        LOG.debug('timestamp %s', time_stamp)
 
-                    tenant_id = decoded['meta']['tenantId']
-                    LOG.debug('tenant id: %s', tenant_id)
+        value = decoded['metric']['value']
+        LOG.debug('value: %s', value)
 
-                    dimensions = {}
-                    if 'dimensions' in decoded['metric']:
-                        for dimension_name in decoded['metric'][
-                            'dimensions']:
-                            dimensions[dimension_name] = (
-                                decoded['metric']['dimensions'][
-                                    dimension_name])
-                            LOG.debug('dimension: %s : %s', dimension_name,
-                                      dimensions[dimension_name])
+        url_encoded_serie_name = (
+            urllib.quote(metric_name.encode('utf8'),
+                         safe='') + '?' + urllib.quote(
+                tenant_id.encode('utf8'), safe='') + '&' + urllib.quote(
+                region.encode('utf8'), safe=''))
 
-                    time_stamp = decoded['metric']['timestamp']
-                    LOG.debug('timestamp %s', time_stamp)
+        for dimension_name in dimensions:
+            url_encoded_serie_name += (
+                '&' + urllib.quote(dimension_name.encode('utf8'),
+                                   safe='') + '=' + urllib.quote(
+                    dimensions[dimension_name].encode('utf8'), safe=''))
 
-                    value = decoded['metric']['value']
-                    LOG.debug('value: %s', value)
+        LOG.debug("url_encoded_serie_name: %s", url_encoded_serie_name)
 
-                    url_encoded_serie_name = (
-                        urllib.quote(metric_name.encode('utf8'), safe='')
-                        + '?' + urllib.quote(tenant_id.encode('utf8'),
-                                             safe='')
-                        + '&' + urllib.quote(region.encode('utf8'),
-                                             safe=''))
+        data = {"points": [[value, time_stamp]],
+                "name": url_encoded_serie_name, "columns": ["value", "time"]}
 
-                    for dimension_name in dimensions:
-                        url_encoded_serie_name += ('&'
-                                                   + urllib.quote(
-                            dimension_name.encode('utf8'), safe='')
-                                                   + '='
-                                                   + urllib.quote(
-                            dimensions[dimension_name].encode('utf8'),
-                            safe=''))
-
-                    LOG.debug("url_encoded_serie_name: %s",
-                              url_encoded_serie_name)
-
-                    data = {"points": [[value, time_stamp]],
-                            "name": url_encoded_serie_name,
-                            "columns": ["value", "time"]}
-
-                    LOG.debug(data)
-                    json_body.append(data)
-
-                    count += 1
-                    if count % self.conf.kafka.metrics_batch_size == 0:
-                        influxdb_client.write_points(json_body)
-                        consumer.commit()
-                        last_flush = datetime.now()
-                        count = 0
-                        json_body = []
-
-        except Exception:
-            LOG.exception(
-                'Persister encountered fatal exception processing metrics')
-            raise
+        LOG.debug(data)
+        return data
 
 
 def main_service():
